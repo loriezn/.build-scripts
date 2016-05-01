b44c8caa7198a9254c2aaa256681c59165171b4c
AffinitySkin <web@affinityskin.com.au>
2016-05-02 08:16:39 +1000

    Create 00-dot-links.sh

---
+++ 00-dot-links.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -sn /home/loki/00-build-scripts/dotfiles/
+ln -sn /home/loki/00-build-scripts/dotfiles/
+ln -sn /home/loki/00-build-scripts/dotfiles/
+ln -sn /home/loki/00-build-scripts/dotfiles/
+ln -sn /home/loki/00-build-scripts/dotfiles/
+ln -sn /home/loki/00-build-scripts/dotfiles/
