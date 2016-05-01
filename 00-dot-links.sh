5f8cfbebd2e781e628c9d96f6a96a5f050c83c1a
AffinitySkin <web@affinityskin.com.au>
2016-05-02 08:22:20 +1000

    Update 00-dot-links.sh

--- 00-dot-links.sh
+++ 00-dot-links.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-ln -sn /home/loki/00-build-scripts/dotfiles/
-ln -sn /home/loki/00-build-scripts/dotfiles/
-ln -sn /home/loki/00-build-scripts/dotfiles/
-ln -sn /home/loki/00-build-scripts/dotfiles/
-ln -sn /home/loki/00-build-scripts/dotfiles/
-ln -sn /home/loki/00-build-scripts/dotfiles/
+dot=/home/loki/00-build-scripts/dotfiles
+loki=/home/loki
+
+cd $loki
+ln -sn $dot/.bash..d
+mv $loki/.config $loki/.config-bu
+ln -sn $dot/.config
+ln -sn $dot/.i3
+ln -sn $dot/.vim
+ln -sn $dot/.weechat
+ln -sn $dot/scripts
+ln -sn $dot/.Xresources
+ln -sn $dot/.bash_profile
+mv $loki/.bashrc $loki/.bashrc.bu
+ln -sn $dot/.bashrc
+ln -sn $dot/.compton.conf
+ln -sn $dot/.gitconfig
+ln -sn $dot/.gitignore
+ln -sn $dot/.gtkre-2.0
+ln -sn $dot/.vimrc
+ln -sn $dot/.xinitrc
