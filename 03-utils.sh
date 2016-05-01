7e3e5438723067f838a963cf7cf83276700299cc
AffinitySkin <web@affinityskin.com.au>
2016-05-02 08:12:08 +1000

    Update 03-utils.sh

--- 03-utils.sh
+++ 03-utils.sh
@@ -48,10 +48,4 @@ apt-get install -y \
 	gvfs-libs \
 	xclip
 
-#echo "Adding neofetch public key"
-#        echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | sudo tee -a /etc/apt/sources.list
-#        curl -L "https://bintray.com/user/downloadSubjectPublicKey?username=bintray" -o Release-neofetch.key && apt-key add Release-neofetch.key && rm Release-neofetch.key
-#echo "Installing neofetch"
-#        apt-get update
-#        apt-get install neofetch
 
