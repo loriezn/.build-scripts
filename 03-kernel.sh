5a748e76cd8821350aa94bfcff051ec816c83534
affinityskin <web@affinityskin.com.au>
2016-07-11 04:29:26 +1000

    update optiplex

--- 03-kernel.sh
+++ 03-kernel.sh
@@ -16,19 +16,4 @@ echo "Updating System Kernel"
 sudo apt-get update
 sudo apt-get install -y bcmwl-kernel-source unzip
 
-cp /home/loki/00-build-pkgs/BCM20702A1_001.002.014.1443.1572.hcd /lib/firmware/brcm/BCM20702A1-0a5c-216f.hcd
-ln -s /lib/firmware/brcm/BCM20702A1-0a5c-216f.hcd /lib/firmware/brcm/BCM20702A0-0a5c-216f.hcd
-
-cd ${MAKEDIR}
-git clone https://github.com/DisplayLink/evdi.git
-cd evdi
-make
-make install
-
-wget http://www.displaylink.com/downloads/file?id=123
-mv file?id=123 displaylink.zip
-unzip displaylin.zip
-
-./displaylink-driver-0.9.68.run --noexec --keep
-
 exit
